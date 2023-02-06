5a70dcd139341b3a83892e15208a384c98834655
Mark Hansen <mhansen@accusoft.com>
2023-02-06 00:17:41 -0500

    Working on layout

--- src/draft-table.js
+++ src/draft-table.js
@@ -1,7 +1,39 @@
 import './app.css'
+import './draft-table.css'
 
-function hi() {
-    alert("hi");
+let cards;
+let filter = [];
+const DATA_URL = "https://snapdata.stonedonkey.com/";
+
+async function getCards() {
+    let result = await(await fetch("https://snapdata.stonedonkey.com/data/snap.json")).json();
+    cards = result.data.cards;
+    filter.card  = cards.card.filter(card => card.released == true && card.energy == 1);
+    sortCards();
+   
 }
 
-window.hi = hi;
+
+function bindCards() {
+
+    filter.card.forEach(c => {
+        
+        let i = document.createElement("img");
+        i.src = DATA_URL + "images/cards/" + c.id + ".webp"; // render card.
+        i.classList.add("table-card")
+        document.getElementById("wrapper").appendChild(i);
+
+         if (c.released == true) 
+             console.log(c.name);
+
+    });
+}
+
+function sortCards() {
+    filter.card.sort((a, b) => a.name > b.name ? 1 : -1)
+    filter.card.sort((a, b) => a.power - b.power);
+    filter.card.sort((a, b) => a.energy - b.energy);
+}
+
+window.getCards = getCards;
+window.bindCards = bindCards;
