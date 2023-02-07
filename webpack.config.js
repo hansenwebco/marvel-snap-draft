794fbf2b1c05dbb0b84652076a72c74044c0d144
Mark Hansen <mhansen@accusoft.com>
2023-02-07 09:37:26 -0500

    updated deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,20 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('node:path');
+
 module.exports = {
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "index.html"), to: "../index.html" },
+        { from: path.resolve(__dirname, "vote.html"), to: "../vote.html" },
+        { from: path.resolve(__dirname, "images"), to: "../images" },
+        { from: path.resolve(__dirname, "sound"), to: "../sound" },
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
   entry: [
     './src/vote.js',
     './src/app.js',
@@ -6,7 +22,7 @@ module.exports = {
     './src/season1.css',
   ],
   output: {
-    path: `${__dirname}/dist`,
+    path: `${__dirname}/dist/scripts`,
     publicPath: '/',
     filename: 'dist.js',
     clean: true,
@@ -16,7 +32,7 @@ module.exports = {
       writeToDisk: true,
     },
     static: {
-      directory: __dirname
+      directory: `${__dirname}/dist`
     }
   },
   module: {
@@ -24,14 +40,14 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader',
-      },  
+      },
       {
         test: /\.css$/,
         loader: 'css-loader',
         options: {
           url: false,
         }
-      },  
+      },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
@@ -41,7 +57,7 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      }      
+      }
     ]
   }
 };
